c28936edd8b04f883058571e09f5eec7fbd8aae8
sunlaud <sunlaud@mail.ru>
2018-04-21 20:21:25 +0300

    Various improvements and refactoring

--- background.js
+++ background.js
@@ -1,28 +1,23 @@
+var scriptsByUrlPattern;
+
+
+function loadConfig() {
+    browser.storage.local.get()
+        .then(configData => scriptsByUrlPattern = configData)
+        .catch(error => console.error("Failed to load extension config data from storage: ", error));
+}
+
+
+loadConfig();
+browser.storage.onChanged.addListener(loadConfig);
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (!changeInfo.url) {
-//         return;
-//     }
-    console.info("onUpdated: ", changeInfo, tab);
-    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-    gettingActiveTab.then((tabs) => {
-        console.info("active: ", tabs)
-    });
-});
-
-
-browser.tabs.onCreated.addListener((tabId, changeInfo, tab) => {
-    //     if (!changeInfo.url) {
-    //         return;
-    //     }
-    console.info("onUpdated: ", changeInfo, tab);
-    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-    gettingActiveTab.then((tabs) => {
-        console.info("active: ", tabs)
-    });
-});
-
-
-
-browser.tabs.onActivated.addListener((activeInfo) => {
-    console.info("activated: ", activated);
+    let url = changeInfo.url;
+    if (url) {
+        let matchedUrlPatterns = Object.keys(scriptsByUrlPattern).filter((urlPattern) => new RegExp(urlPattern).test(url));
+        matchedUrlPatterns.forEach(urlPattern => {
+            let script = scriptsByUrlPattern[urlPattern];
+            console.info(`page url '${url}' matched pattern '${urlPattern}' - injecting userscipt into page...`);
+            browser.tabs.executeScript(tabId, { code: script });
+        })
+    }
 });
--- options.js
+++ options.js
@@ -1,28 +1,26 @@
-'use strict'
-
-var mockUrls = {"gg" : "tt", "yy" : "oo"};
-mockUrls = {};
-var browser = { storage: {local: {get: () => {return {then: (callback) => callback(mockUrls)}}}}}
-
 var scriptsByUrl;
 
 var urlSelect = document.querySelector("#urls");
 var scriptInput = document.querySelector("#script");
 var urlInput = document.querySelector("#url");
 var newButton = document.querySelector("#new");
-var saveButton = document.querySelector("#save");
+var exportButton = document.querySelector("#export");
 var removeButton = document.querySelector("#remove");
-
-
+var form = document.querySelector("form");
+/*
+var emalutadData = {};
+var browser = { storage: {local: {
+    get: () => {return {then: callback => {callback(emalutadData); return {catch: (callback) => callback("error")}}}},
+    set: (data) => {emalutadData = data; return {then: callback => callback()}}
+}}}
+*/
 
 function init() {
-    console.info('loading data');
     browser.storage.local.get().then((data) => {
-        console.info('got data: ', data);
         scriptsByUrl = data;
         Object.keys(scriptsByUrl).forEach(addUrlToSelect);
         selectUrl(0);
-    });
+    }).catch((error) => alert("Failed to load config data: " + error));
 }
 
 function addUrlToSelect(url) {
@@ -34,50 +32,7 @@ function addUrlToSelect(url) {
 
 function selectUrl(indexToSelect) {
     urlSelect.selectedIndex = indexToSelect;
-    selectedUrlChanged();
-}
-
-function selectedUrlChanged() {
-    let url = urlSelect.value;
-    console.debug("url selected: ", url);
-    removeButton.disabled = !url;
-    urlInput.value = url;
-    scriptInput.value = scriptsByUrl[url] || "";
-}
-
-function save() {
-    let oldUrl = urlSelect.value;
-    let url = urlInput.value;
-    let script = scriptInput.value;
-    console.info("saving", "oldUrl=", oldUrl, "newUrl=", url);
-    if (!url) {
-        alert("Error: url is required")
-        return;
-    }
-    if (!script) {
-        alert("Error: script is required")
-        return;
-    }
-    if (oldUrl == url) {
-        setSelectedUrlValueTo(url);
-    } else {
-        if (scriptsByUrl[url]) {
-            alert("Error: script for url '" + url + "' is already present!");
-            return;
-        }
-        if (oldUrl) {
-            delete scriptsByUrl[oldUrl];
-            urlSelect.options.remove(urlSelect.selectedIndex);
-        }
-        console.debug("adding url to selecte...");
-        addUrlToSelect(url);
-        urlSelect.selectedIndex = urlSelect.options.length - 1;
-        removeButton.disabled = false;
-    }
-    scriptsByUrl[url] = script;
-
-    console.log("saving ", url + " => " + script);
-    browser.storage.local.set(scriptsByUrl);
+    urlSelect.onchange();
 }
 
 function setSelectedUrlValueTo(url) {
@@ -86,26 +41,73 @@ function setSelectedUrlValueTo(url) {
     option.text = url;
 }
 
-function newUrl() {
-    selectUrl(-1);
-}
+function saveUserscript(event) {
+    let oldUrl = urlSelect.value;
+    let url = urlInput.value;
+    let script = scriptInput.value;
+    console.debug("saving: ", oldUrl, url, script);
 
-function remove() {
-    let index = urlSelect.selectedIndex;
-    let url = urlSelect.options[index].value;
-    if (!confirm("Really delete url '" + url + "'?")) {
+    event.preventDefault();
+    if (!url || !script) {
         return;
     }
-    urlSelect.options.remove(index);
-    delete scriptsByUrl[url];
-    browser.storage.local.set(scriptsByUrl);
-    selectUrl(0);
+    if (oldUrl != url) {
+        if (scriptsByUrl[url]) {
+            alert(`Error: script for url '${url}' is already present!`);
+            return;
+        }
+        if (oldUrl) {
+            setSelectedUrlValueTo(url);
+            delete scriptsByUrl[oldUrl];
+            browser.storage.local.remove(oldUrl).catch(error => alert("Failed to save config data: " + error));
+        } else {
+            addUrlToSelect(url);
+            urlSelect.selectedIndex = urlSelect.options.length - 1;
+        }
+
+        removeButton.disabled = false;
+    }
+    scriptsByUrl[url] = script;
+    browser.storage.local.set(scriptsByUrl).catch(error => alert("Failed to save config data: " + error));
 }
 
-document.addEventListener('DOMContentLoaded', init);
-urlSelect.onchange = selectedUrlChanged;
-saveButton.onclick = save;
-removeButton.onclick = remove;
-newButton.onclick = newUrl;
+function removeSelectedUserscript() {
+    let index = urlSelect.selectedIndex;
+    let url = urlSelect.options[index].value;
 
-console.clear();
+    return browser.storage.local.remove(url).then(() => {
+        urlSelect.options.remove(index);
+        delete scriptsByUrl[url];
+    }).catch(error => alert("Failed to save config data: " + error));
+}
+
+function exportSettings() {
+    let configDataAsUrl = "data:text/json," + encodeURIComponent(JSON.stringify(scriptsByUrl, null, "  "));
+    let something = window.open(configDataAsUrl, "_blank");
+    something.focus();
+}
+
+urlSelect.onchange = () => {
+    let url = urlSelect.value;
+    removeButton.disabled = !url;
+    urlInput.value = url;
+    scriptInput.value = scriptsByUrl[url] || "";
+};
+
+newButton.onclick = (event) => {
+    event.preventDefault();
+    selectUrl(-1);
+};
+
+removeButton.onclick = (event) => {
+    event.preventDefault();
+    if (confirm(`Really delete script for '${url}'?`)) {
+        removeSelectedUserscript().then(() => selectUrl(0));
+    }
+};
+
+form.onsubmit = saveUserscript;
+exportButton.onclick = exportSettings;
+
+document.addEventListener('DOMContentLoaded', init);
+
